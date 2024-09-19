5ce08654a002e3b509076d9cbd47cad11a768bfb
André Freitas <145010889+AMfr3it4s@users.noreply.github.com>
2024-09-19 10:16:28 +0100

    Navigation Between Screens

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,7 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_application/firebase_options.dart';
 import 'package:flutter_application/views/register_view.dart';
 
 // LoginPage
@@ -33,100 +31,76 @@ class _LoginViewState extends State<LoginView> {
 
   @override
   Widget build(BuildContext context) {
-    final colorScheme = Theme.of(context).colorScheme;
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-        backgroundColor: colorScheme.secondary,
-        foregroundColor: colorScheme.onPrimary,
-      ),
-      body: FutureBuilder(
-        future: Firebase.initializeApp(
-          options: DefaultFirebaseOptions.currentPlatform,
+    return Column(
+      children: [
+        TextField(
+          controller: _email,
+          enableSuggestions: false,
+          autocorrect: false,
+          keyboardType: TextInputType.emailAddress,
+          decoration: const InputDecoration(hintText: "Enter Your Email"),
         ),
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.done:
-              return Column(
-                children: [
-                  TextField(
-                    controller: _email,
-                    enableSuggestions: false,
-                    autocorrect: false,
-                    keyboardType: TextInputType.emailAddress,
-                    decoration:
-                        const InputDecoration(hintText: "Enter Your Email"),
-                  ),
-                  TextField(
-                    controller: _password,
-                    obscureText: true,
-                    enableSuggestions: false,
-                    autocorrect: false,
-                    decoration:
-                        const InputDecoration(hintText: "Enter Your Password"),
-                  ),
-                  TextButton(
-                      onPressed: () async {
-                        final email = _email.text;
-                        final password = _password.text;
-                        final scaffoldMessengerState =
-                            ScaffoldMessenger.of(context);
-                        try {
-                          await FirebaseAuth.instance
-                              .signInWithEmailAndPassword(
-                                  email: email, password: password);
-                          if (mounted) {
-                            scaffoldMessengerState.showSnackBar(
-                              const SnackBar(
-                                content: Text("Login Success"),
-                                backgroundColor: Colors.green,
-                              ),
-                            );
-                          }
-                        } on FirebaseAuthException catch (e) {
-                          String message = '';
-                          if (e.code == 'invalid-email') {
-                            message = 'No user found with this email.';
-                          } else if (e.code == 'invalid-credential') {
-                            message = 'Incorrect password.';
-                          } else {
-                            message = 'Login failed. Please try again.';
-                          }
+        TextField(
+          controller: _password,
+          obscureText: true,
+          enableSuggestions: false,
+          autocorrect: false,
+          decoration: const InputDecoration(hintText: "Enter Your Password"),
+        ),
+        TextButton(
+            onPressed: () async {
+              final email = _email.text;
+              final password = _password.text;
+              final scaffoldMessengerState = ScaffoldMessenger.of(context);
+              try {
+                await FirebaseAuth.instance.signInWithEmailAndPassword(
+                    email: email, password: password);
+                if (mounted) {
+                  scaffoldMessengerState.showSnackBar(
+                    const SnackBar(
+                      content: Text("Login Success"),
+                      backgroundColor: Colors.green,
+                    ),
+                  );
+                }
+              } on FirebaseAuthException catch (e) {
+                String message = '';
+                if (e.code == 'invalid-email') {
+                  message = 'No user found with this email.';
+                } else if (e.code == 'invalid-credential') {
+                  message = 'Incorrect password.';
+                } else {
+                  message = 'Login failed. Please try again.';
+                }
 
-                          // Show the message in a SnackBar
-                          scaffoldMessengerState.showSnackBar(
-                            SnackBar(
-                              content: Text(message),
-                              backgroundColor: Colors
-                                  .red, // Optional: to change background color
-                            ),
-                          );
-                        } catch (e) {
-                          // General error message
-                          scaffoldMessengerState.showSnackBar(
-                            SnackBar(
-                              content: Text('Error: $e'),
-                              backgroundColor: Colors.red,
-                            ),
-                          );
-                        }
-                      },
-                      child: const Text("Login")),
-                  TextButton(
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const RegisterView()));
-                      },
-                      child: const Text("Register Now"))
-                ],
-              );
-            default:
-              return const Text('Loading...');
-          }
-        },
-      ),
+                // Show the message in a SnackBar
+                scaffoldMessengerState.showSnackBar(
+                  SnackBar(
+                    content: Text(message),
+                    backgroundColor:
+                        Colors.red, // Optional: to change background color
+                  ),
+                );
+              } catch (e) {
+                // General error message
+                scaffoldMessengerState.showSnackBar(
+                  SnackBar(
+                    content: Text('Error: $e'),
+                    backgroundColor: Colors.red,
+                  ),
+                );
+              }
+            },
+            child: const Text("Login")),
+        TextButton(
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const RegisterView()));
+            },
+            child: const Text("Register Now"))
+      ],
     );
   }
 }
--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,7 +1,5 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter_application/firebase_options.dart';
 
 //RegisterPage
 class RegisterView extends StatefulWidget {
@@ -31,97 +29,73 @@ class _RegisterViewState extends State<RegisterView> {
 
   @override
   Widget build(BuildContext context) {
-    final colorScheme = Theme.of(context).colorScheme;
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Register'),
-        backgroundColor: colorScheme.secondary,
-        foregroundColor: colorScheme.onPrimary,
-      ),
-      body: FutureBuilder(
-        future: Firebase.initializeApp(
-          options: DefaultFirebaseOptions.currentPlatform,
+    return Column(
+      children: [
+        TextField(
+          controller: _email,
+          enableSuggestions: false,
+          autocorrect: false,
+          keyboardType: TextInputType.emailAddress,
+          decoration: const InputDecoration(hintText: "Enter Your Email"),
         ),
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.done:
-              return Column(
-                children: [
-                  TextField(
-                    controller: _email,
-                    enableSuggestions: false,
-                    autocorrect: false,
-                    keyboardType: TextInputType.emailAddress,
-                    decoration:
-                        const InputDecoration(hintText: "Enter Your Email"),
+        TextField(
+          controller: _password,
+          obscureText: true,
+          enableSuggestions: false,
+          autocorrect: false,
+          decoration: const InputDecoration(hintText: "Enter Your Password"),
+        ),
+        TextButton(
+            onPressed: () async {
+              final email = _email.text;
+              final password = _password.text;
+              final scaffoldMessengerState = ScaffoldMessenger.of(context);
+              try {
+                await FirebaseAuth.instance.createUserWithEmailAndPassword(
+                    email: email, password: password);
+                scaffoldMessengerState.showSnackBar(
+                  const SnackBar(
+                    content: Text("Register Success"),
+                    backgroundColor: Colors.green,
                   ),
-                  TextField(
-                    controller: _password,
-                    obscureText: true,
-                    enableSuggestions: false,
-                    autocorrect: false,
-                    decoration:
-                        const InputDecoration(hintText: "Enter Your Password"),
-                  ),
-                  TextButton(
-                      onPressed: () async {
-                        final email = _email.text;
-                        final password = _password.text;
-                        final scaffoldMessengerState =
-                            ScaffoldMessenger.of(context);
-                        try {
-                          await FirebaseAuth.instance
-                              .createUserWithEmailAndPassword(
-                                  email: email, password: password);
-                          scaffoldMessengerState.showSnackBar(
-                            const SnackBar(
-                              content: Text("Register Success"),
-                              backgroundColor: Colors.green,
-                            ),
-                          );
-                        } on FirebaseAuthException catch (e) {
-                          if (e.code == 'weak-password') {
-                            scaffoldMessengerState.showSnackBar(
-                              const SnackBar(
-                                content: Text('Weak-Password'),
-                                backgroundColor: Colors.red,
-                              ),
-                            );
-                          }
-                          if (e.code == 'email-already-in-use') {
-                            scaffoldMessengerState.showSnackBar(
-                              const SnackBar(
-                                content: Text('Email Already Registered'),
-                                backgroundColor: Colors.red,
-                              ),
-                            );
-                          }
+                );
+              } on FirebaseAuthException catch (e) {
+                if (e.code == 'weak-password') {
+                  scaffoldMessengerState.showSnackBar(
+                    const SnackBar(
+                      content: Text('Weak-Password'),
+                      backgroundColor: Colors.red,
+                    ),
+                  );
+                }
+                if (e.code == 'email-already-in-use') {
+                  scaffoldMessengerState.showSnackBar(
+                    const SnackBar(
+                      content: Text('Email Already Registered'),
+                      backgroundColor: Colors.red,
+                    ),
+                  );
+                }
 
-                          if (e.code == 'invalid-email') {
-                            scaffoldMessengerState.showSnackBar(
-                              const SnackBar(
-                                content: Text('Invalid Email'),
-                                backgroundColor: Colors.red,
-                              ),
-                            );
-                          } else {
-                            scaffoldMessengerState.showSnackBar(
-                              const SnackBar(
-                                content: Text('Failed to Register'),
-                                backgroundColor: Colors.red,
-                              ),
-                            );
-                          }
-                        }
-                      },
-                      child: const Text("Register")),
-                ],
-              );
-            default:
-              return const Text('Loading...');
-          }
-        },
-      ),
+                if (e.code == 'invalid-email') {
+                  scaffoldMessengerState.showSnackBar(
+                    const SnackBar(
+                      content: Text('Invalid Email'),
+                      backgroundColor: Colors.red,
+                    ),
+                  );
+                } else {
+                  scaffoldMessengerState.showSnackBar(
+                    const SnackBar(
+                      content: Text('Failed to Register'),
+                      backgroundColor: Colors.red,
+                    ),
+                  );
+                }
+              }
+            },
+            child: const Text("Register")),
+      ],
     );
   }
 }
