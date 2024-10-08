cc5b661430c5bbf4bc8aa0e5d2c471303351dbeb
André Freitas <amfreitas23@ua.pt>
2024-10-08 16:26:11 +0100

    New Radial UI

--- lib/views/activity_view.dart
+++ lib/views/activity_view.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:pedometer/pedometer.dart';
 import 'package:permission_handler/permission_handler.dart';
-import 'package:percent_indicator/circular_percent_indicator.dart';
-import 'package:fl_chart/fl_chart.dart';
 import 'dart:async';
+import 'package:radial_progress/radial_progress.dart';
+import 'package:emojis/emojis.dart';
 
 class ActivityView extends StatefulWidget {
   const ActivityView({super.key});
@@ -14,18 +14,20 @@ class ActivityView extends StatefulWidget {
 
 class _ActivityViewState extends State<ActivityView> {
   late Stream<StepCount> _stepCountStream;
-  int _totalSteps = 0; // Contagem de passos do dia atual
-  List<int> _stepsHistory = []; // Histórico de passos por dia
+  int _totalSteps = 0; 
+  final List<int> _stepsHistory = []; 
   final int _goalSteps = 10000; // Meta
   double _distanceTraveled = 0.0; // em km
   double _caloriesBurned = 0.0; // em kcal
   late Timer _resetTimer;
+  double _percent = 0.0;
+  
 
   @override
   void initState() {
     super.initState();
     _requestPermissions();
-    _startResetTimer(); // Inicia o temporizador para resetar passos
+    _startResetTimer(); 
   }
 
   void _requestPermissions() async {
@@ -46,28 +48,34 @@ class _ActivityViewState extends State<ActivityView> {
           _totalSteps = stepCount.steps;
           _distanceTraveled = _totalSteps * 0.0008;
           _caloriesBurned = _totalSteps * 0.05;
-          if (_stepsHistory.isEmpty || DateTime.now().day != DateTime.now().add(Duration(days: -1)).day) {
-            // Armazena passos e reinicia contagem diária se for um novo dia
+          if (_stepsHistory.isEmpty || DateTime.now().day != DateTime.now().add(const Duration(days: -1)).day) {
             _stepsHistory.add(_totalSteps);
-            _totalSteps = 0; // Reinicia a contagem de passos do dia
+            _totalSteps = 0; 
           }
+          _updatePercent();
         });
       },
       onError: (error) {
-        print("Error listening Steps: $error");
+        
       },
     );
   }
 
+  void _updatePercent() {
+  setState(() {
+    _percent = _totalSteps /_goalSteps;
+    print("Percent: $_percent");
+  });
+}
+
   void _startResetTimer() {
-    // Define o temporizador para resetar a contagem de passos à meia-noite
-    _resetTimer = Timer.periodic(Duration(seconds: 1), (timer) {
+    _resetTimer = Timer.periodic(const Duration(seconds: 1), (timer) {
       if (DateTime.now().hour == 0 && DateTime.now().minute == 0) {
         setState(() {
-          _stepsHistory.add(_totalSteps); // Armazena o total de passos antes de resetar
-          _totalSteps = 0; // Reinicia a contagem de passos do dia
-          _distanceTraveled = 0.0; // Reinicia a distância percorrida
-          _caloriesBurned = 0.0; // Reinicia as calorias queimadas
+          _stepsHistory.add(_totalSteps); 
+          _totalSteps = 0; 
+          _distanceTraveled = 0.0; 
+          _caloriesBurned = 0.0; 
         });
       }
     });
@@ -75,7 +83,7 @@ class _ActivityViewState extends State<ActivityView> {
 
   @override
   void dispose() {
-    _resetTimer.cancel(); // Cancela o temporizador ao descartar o widget
+    _resetTimer.cancel(); 
     super.dispose();
   }
 
@@ -85,7 +93,7 @@ class _ActivityViewState extends State<ActivityView> {
       backgroundColor: const Color.fromRGBO(239, 235, 206, 1),
       appBar: AppBar(
         backgroundColor: const Color.fromRGBO(239, 235, 206, 1),
-        title: const Text('Pedometer App'),
+        title: const Text("Today's Activity"),
         centerTitle: true,
       ),
       body: Padding(
@@ -93,39 +101,55 @@ class _ActivityViewState extends State<ActivityView> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            CircularPercentIndicator(
-              radius: 150.0,
-              lineWidth: 15.0,
-              percent: _totalSteps / _goalSteps,
-              center: Column(
+            RadialProgressWidget(
+              percent: _percent,
+              diameter: 180,
+              bgLineColor: const Color.fromRGBO(47, 62, 70, 0.2),
+              progressLineColors: const  [ Color.fromRGBO(47, 62, 70, 1)],
+              progressLineWidth: 16,
+              startAngle: StartAngle.top,
+              centerChild:  Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text(
-                    "$_totalSteps",
-                    style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 28),
-                  ),
-                  const SizedBox(height: 4),
-                  const Text(
-                    "Today's Steps",
-                    style: TextStyle(fontSize: 16),
-                  ),
-                ],
+                   Text(
+                  _percent < 0.2 ? "You Can do it!" : _percent < 0.5 ? "Good Job!" : _percent < 0.7 ? "Great Job!" : _percent == 1 ? "You are on Fire" : "Excellent",
+                  textAlign: TextAlign.center,
+                  maxLines: 2,
+                  style: const TextStyle(
+                  fontWeight: FontWeight.bold
               ),
-              progressColor: Colors.green,
-              backgroundColor: Colors.grey,
-            ),
+              ), 
+              Text(_percent < 0.2 ? Emojis.winkingFace : _percent < 0.5 ? Emojis.grinningFace : _percent <0.7 ? Emojis.upsideDownFace : _percent == 1? Emojis.fire : Emojis.astonishedFace,
+                  textAlign: TextAlign.center,
+                  maxLines: 1,
+                  style: const TextStyle(
+                    fontSize: 30,
+                  ),
+              ),
+              const  SizedBox(height: 10),
+              Text("$_totalSteps",
+                  maxLines: 2,
+                  style: const TextStyle(
+                  fontWeight: FontWeight.bold)
+              ),
+              ],
+                
+              )
+               
+              ),
             const SizedBox(height: 20),
             _buildStatistics(),
             const SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
                 setState(() {
-                  _totalSteps += 100; // Adiciona 10 passos ao total
+                  _totalSteps += 100; 
                   _distanceTraveled = _totalSteps * 0.0008;
                   _caloriesBurned = _totalSteps * 0.05;
+                  _updatePercent();
                 });
               },
-              child: Text("Test Add Steps"),
+              child: const Text("Test Add Steps"),
             ),
           ],
         ),
